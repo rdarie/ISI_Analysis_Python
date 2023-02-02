2f77b56fd7232ddb8a7f688015bf3b92c2a30b93
rdarie <radu_darie@brown.edu>
2023-02-02 11:52:21 -0500

    make installable

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='isicpy',
+    version='0.1',
+    packages=find_packages(),
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    # install_requires=install_requires,
+    # long_description=open('README.txt').read(),
+)
